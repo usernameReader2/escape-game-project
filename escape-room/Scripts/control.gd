8347d017d738651023c76f008a1dff4acb80c92a
usernameReader2 <izhong@students.mags.school.nz>
2026-06-05 09:38:27 +1200

    Starting

---
+++ escape-room/Scripts/control.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _go_left() -> void:
+	pass # Replace with function body.
+
+func _go_right() -> void:
+	pass # Replace with function body.
